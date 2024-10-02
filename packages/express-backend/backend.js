7c9b96decc2253d4906d15b0c3ebffa0a8ff41d8
Glenn1504 <glenncarvalho.01@gmail.com>
2024-10-02 15:36:54 -0700

    Added /users/:id route to retrieve users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,6 +46,9 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -54,6 +57,17 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+// Define the /users/:id route to return a specific user by id
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
