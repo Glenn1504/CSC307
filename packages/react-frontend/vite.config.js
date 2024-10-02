cb67893951f4ba62e142e5d687df23357f750539
Glenn1504 <glenncarvalho.01@gmail.com>
2024-10-02 15:09:18 -0700

    Added .gitignore in root to exclude node_modules

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'packages/react-frontend', // Set the root directory for Vite to the react-frontend folder
   plugins: [react()],
+  build: {
+    outDir: '../../dist' // Output the build to the root folder
+  }
 })
